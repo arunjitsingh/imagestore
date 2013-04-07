f39e066e4d8f6723f90a222c82cab40ae0b24437
Arunjit Singh <arunjit@me.com>
2013-04-07 14:19:27 +0530

    update config

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 # Copyright 2013 Arunjit Singh. All Rights Reserved.
 
-from google.appengine.api import app_identity
+import os
 
 from ajpy.appengine import context
+
 
 PRODUCTION = context.IS_PRODUCTION
 DEBUGGING = not PRODUCTION
 
-APP_ID = app_identity.get_application_id()
+ROOT = os.path.dirname(__file__)
+
+WEBAPP2 = {
+    'webapp2_extras.jinja2': {
+        'template_path': ['templates'],
+    },
+}
